af21d43978da7ff42bd6735908a0137fa6267a57
emir <froydenzi@gmail.com>
2020-10-10 01:09:06 +0200

    Dodano osam fragmenata
    NeedLoginFragment.kt
    NemaPodatakaFragment.kt
    NemaPromoFragment.kt
    NemaTenderaFragment.kt
    NemaVijestiFragment.kt
    OopsPaketFragment.kt
    OdrzavanjeFragment.kt
    NoConnectionFragment.kt
    
    dodane margine na sva back dugmad da bude 30x30(top & left),kao i dimenzija istog 20x20

---
+++ app/src/main/java/com/mezet/actaandroid/NemaPromoFragment.kt
@@ -0,0 +1,11 @@
+package com.mezet.actaandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NemaPromoFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_nema_promo)
+    }
+}
---
+++ app/src/main/java/com/mezet/actaandroid/OopsPaketFragment.kt
@@ -0,0 +1,11 @@
+package com.mezet.actaandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OopsPaketFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_oops_paket)
+    }
+}
