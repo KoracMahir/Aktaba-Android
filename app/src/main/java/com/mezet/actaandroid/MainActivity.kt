e53da5a6e59f28f7b701f4e8eca5b1292610188a
KoracMahir <koracdevelopers@gmail.com>
2020-10-09 19:41:28 +0200

    first commit

---
+++ app/src/main/java/com/mezet/actaandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mezet.actaandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_kursna_lista)
+    }
+}
