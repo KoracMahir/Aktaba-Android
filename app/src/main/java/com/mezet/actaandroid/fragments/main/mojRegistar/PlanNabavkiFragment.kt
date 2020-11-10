12d589682697ac9a8678deca7b410457e6d4ef23
froydenzi <froydenzi@gmail.com>
2020-11-10 23:29:35 +0100

    završen čitav registar

---
+++ app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/PlanNabavkiFragment.kt
@@ -0,0 +1,18 @@
+package com.mezet.actaandroid.fragments.main.mojRegistar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mezet.actaandroid.R
+
+class PlanNabavkiFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plan_nabavki, container, false)
+    }
+}
