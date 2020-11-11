95612c1938a9ee3960960b4a7f529429cedcbaef
Mahir <mahirkorac0@gmail.com>
2020-11-11 13:38:04 +0100

    od ovog pocinjemo sad

--- app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/sections/PressClipingFragment.kt
+++ app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/sections/PressClipingFragment.kt
@@ -1,4 +1,4 @@
-package com.mezet.actaandroid.fragments.main.mojRegistar
+package com.mezet.actaandroid.fragments.main.mojRegistar.sections
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.mezet.actaandroid.R
 
-class UpravaRegistarFragment : Fragment() {
+class PressClipingFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_uprava_registar, container, false)
+        return inflater.inflate(R.layout.fragment_press_cliping, container, false)
     }
 
 }
--- app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/sections/UcesceNaTenderimaFragment.kt
+++ app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/sections/UcesceNaTenderimaFragment.kt
@@ -1,4 +1,4 @@
-package com.mezet.actaandroid.fragments.main.mojRegistar
+package com.mezet.actaandroid.fragments.main.mojRegistar.sections
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.mezet.actaandroid.R
 
-class PrijaviGreskuFragment : Fragment() {
+class UcesceNaTenderimaFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prijavi_gresku, container, false)
+        return inflater.inflate(R.layout.fragment_ucesce_na_tenderima, container, false)
     }
 
 }
