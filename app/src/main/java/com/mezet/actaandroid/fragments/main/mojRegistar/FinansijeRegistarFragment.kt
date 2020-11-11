b4a484f1eca885268375a84927a5469ee2c16e67
Mahir <mahirkorac0@gmail.com>
2020-11-11 12:49:18 +0100

    cmm

--- app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/FinansijeRegistarFragment.kt
+++ app/src/main/java/com/mezet/actaandroid/fragments/main/mojRegistar/FinansijeRegistarFragment.kt
@@ -14,7 +14,7 @@ class FinansijeRegistarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_finansije_registar, container, false)
+        return inflater.inflate(R.layout.fragment_finansije_registar1, container, false)
     }
 
 }
