c5819855e469553dec2329ee91a9394f66b34328
froydenzi <froydenzi@gmail.com>
2020-11-17 21:55:30 +0100

    dodan fragment TenderiPretragaNakonSelekcijeFragment.kt

---
+++ app/src/main/java/com/mezet/actaandroid/fragments/main/tenderi/TenderiPretragaNakonSelekcijeFragment.kt
@@ -0,0 +1,24 @@
+package com.mezet.actaandroid.fragments.main.tenderi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mezet.actaandroid.R
+
+class TenderiPretragaNakonSelekcijeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_tenderi_pretraga_nakon_selekcije,
+            container,
+            false
+        )
+    }
+
+}
